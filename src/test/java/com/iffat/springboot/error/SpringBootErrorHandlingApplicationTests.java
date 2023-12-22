05de4eb1cd5fbb389896b98ab739e5be7280a1f1
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-22 09:34:01 +0700

    01. Starter Project

---
+++ src/test/java/com/iffat/springboot/error/SpringBootErrorHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iffat.springboot.error;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootErrorHandlingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
