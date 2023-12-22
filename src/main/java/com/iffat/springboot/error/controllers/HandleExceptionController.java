bb4dcb3ec6e8517906e5a993e36c46232ce50d58
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-22 10:31:47 +0700

    06. Customizing error page for NumberFormatException

--- src/main/java/com/iffat/springboot/error/controllers/HandleExceptionController.java
+++ src/main/java/com/iffat/springboot/error/controllers/HandleExceptionController.java
@@ -4,10 +4,13 @@ import com.iffat.springboot.error.models.Error;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class HandleExceptionController {
@@ -34,5 +37,18 @@ public class HandleExceptionController {
         error.setStatus(HttpStatus.NOT_FOUND.value());
 
         return ResponseEntity.status(404).body(error);
+    }
+
+    @ExceptionHandler(NumberFormatException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, Object> numberFormatException(Exception exception) {
+
+        Map<String , Object> error = new HashMap<>();
+        error.put("data", new Date());
+        error.put("error", "Invalid format Number");
+        error.put("message", exception.getMessage());
+        error.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+
+        return error;
     }
 }
