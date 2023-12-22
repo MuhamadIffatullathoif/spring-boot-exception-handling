f85bef703c61114099bb60ec11068b6c1761544d
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2023-12-22 09:48:59 +0700

    04. Adding handling method for exception handling

--- src/main/java/com/iffat/springboot/error/controllers/HandleExceptionController.java
+++ src/main/java/com/iffat/springboot/error/controllers/HandleExceptionController.java
@@ -1,15 +1,25 @@
 package com.iffat.springboot.error.controllers;
 
+import com.iffat.springboot.error.models.Error;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Date;
 
 @RestControllerAdvice
 public class HandleExceptionController {
 
     @ExceptionHandler({ArithmeticException.class})
-    public ResponseEntity<String> divideByZero(Exception exception) {
+    public ResponseEntity<Error> divideByZero(Exception exception) {
 
-        return ResponseEntity.internalServerError().body("500 ERROR");
+        Error error = new Error();
+        error.setDate(new Date());
+        error.setError("Error division by zero");
+        error.setMessage(exception.getMessage());
+        error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+
+        return ResponseEntity.internalServerError().body(error);
     }
 }
